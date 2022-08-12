3c1ad4b2466a6d1208c090f3d302f8fa2e2c23ca
hendrawijayasubandi <hendra.w.subandi@gmail.com>
2022-08-12 12:57:53 +0700

    update main activity

--- app/src/main/java/com/hendra/barvolume/MainActivity.kt
+++ app/src/main/java/com/hendra/barvolume/MainActivity.kt
@@ -3,6 +3,9 @@ package com.hendra.barvolume
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
