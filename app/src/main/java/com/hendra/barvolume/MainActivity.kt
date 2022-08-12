4754535de7a62bf4f52f06242afc4316b6a25cfa
hendrawijayasubandi <hendra.w.subandi@gmail.com>
2022-08-12 17:17:51 +0700

    update terakhir

--- app/src/main/java/com/hendra/barvolume/MainActivity.kt
+++ app/src/main/java/com/hendra/barvolume/MainActivity.kt
@@ -15,6 +15,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btnCalculate: Button
     private lateinit var tvResult: TextView
 
+    companion object {
+        private const val STATE_RESULT = "state_result"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,6 +30,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         tvResult = findViewById(R.id.tv_result)
 
         btnCalculate.setOnClickListener(this)
+
+        if (savedInstanceState != null) {
+            val result = savedInstanceState.getString(STATE_RESULT)
+            tvResult.text = result
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_RESULT, tvResult.text.toString())
     }
 
     override fun onClick(v: View?) {
@@ -33,6 +47,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             val inputLength = edtLength.text.toString().trim()
             val inputWidth = edtWidth.text.toString().trim()
             val inputHeight = edtHeight.text.toString().trim()
+
+            var isEmptyFields = false
+            if (inputLength.isEmpty()) {
+                isEmptyFields = true
+                edtLength.error = "Field ini tidak boleh kosong"
+            }
+            if (inputWidth.isEmpty()) {
+                isEmptyFields = true
+                edtWidth.error = "Field ini tidak boleh kosong"
+            }
+            if (inputHeight.isEmpty()) {
+                isEmptyFields = true
+                edtHeight.error = "Field ini tidak boleh kosong"
+            }
+
             val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
             tvResult.text = volume.toString()
         }
